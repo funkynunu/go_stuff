b75afb235c7d5324d24cd14259d354bf74406374
Allen Lister <allen.lister@sap.com>
2022-11-09 09:35:05 +0100

    Added leapyear algorythm

---
+++ leapyear/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// import (
+// 	"github.com/funkynunu/go_rest_api/pkg/leapyear"
+// )
+
+func main() {
+	if IsLeapYear(1996) {
+		fmt.Println("1996 is a leap year")
+	} else {
+		fmt.Println("1996 is not a leap year")
+	}
+	if IsLeapYear(2000) {
+		fmt.Println("2000 is a leap year")
+	} else {
+		fmt.Println("2000 is not a leap year")
+	}
+	if IsLeapYear(1900) {
+		fmt.Println("1900 is a leap year")
+	} else {
+		fmt.Println("1900 is not a leap year")
+	}
+	if IsLeapYear(1997) {
+		fmt.Println("1997 is a leap year")
+	} else {
+		fmt.Println("1997 is not a leap year")
+	}
+}
+
+func IsLeapYear(year int) bool {
+	// Write some code here to pass the test suite.
+	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
+		return true
+		// if b_year100 == 0 {
+		// 	if b_year400 == 0 {
+		// 		return true
+		// 	}
+		// 	return false
+		// }
+		// return true
+	}
+	return false
+}
